882ca2088a0feacf88a1ef2e412b55a7fd464c45
OMAILLOT <72554042+OMAILLOT@users.noreply.github.com>
2022-02-21 17:07:44 +0100

    Add asset and map

--- Server.js
+++ Server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.use("/static", express.static('./static/'));
+app.use("/Assets", express.static('./Assets/'));
 
 app.listen(8080, () => {
-  console.log('Server listening on http://localhost:8080');
+    console.log('Server listening on http://localhost:8080');
 });
--- static/game.js
+++ static/game.js
@@ -1,17 +1,79 @@
-const config = {
-  width: 800,
-  height: 600,
-  type: Phaser.AUTO,
-  audio: {
-    disableWebAudio: true
-  },
-  physics: {
-    default: 'arcade',
-    arcade: {
-      fps: 60,
-      gravity: {y : 0},
+class Game extends Phaser.Scene {
+    mapSize = 0;
+
+    constructor() {
+        super();
+        this.moveCam = false;
     }
-  },
+
+    preload() {
+        this.load.image('bg', '../Assets/BackGround/BlankBG.png');
+        this.load.image('block', '../Assets/BedTable.png');
+    }
+
+    create() {
+        // game.scale.resize(window.innerWidth - 50, window.innerHeight - 50);
+        this.cameras.main.setBounds(0, 0, 3000, 2000);
+
+        for (let x = 0; x < 2; x++) {
+            this.add.image(1500 * x, 0, 'bg').setOrigin(0);
+            this.add.image(1500 * x, 1500, 'bg').setOrigin(0);
+
+        }
+
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.add.image(750, 750, 'block');
+        this.player = this.physics.add.image(1500, 1500, 'block');
+        this.cameras.main.startFollow(this.player, true);
+        this.cameras.main.setZoom(1.5);
+    }
+
+    update() {
+        const cam = this.cameras.main;
+
+        this.player.setVelocity(0);
+
+        if (this.moveCam) {
+            if (this.cursors.left.isDown) {
+                cam.scrollX -= 4;
+            } else if (this.cursors.right.isDown) {
+                cam.scrollX += 4;
+            }
+
+            if (this.cursors.up.isDown) {
+                cam.scrollY -= 4;
+            } else if (this.cursors.down.isDown) {
+                cam.scrollY += 4;
+            }
+        } else {
+            if (this.cursors.left.isDown) {
+                this.player.setVelocityX(-400);
+            } else if (this.cursors.right.isDown) {
+                this.player.setVelocityX(400);
+            }
+
+            if (this.cursors.up.isDown) {
+                this.player.setVelocityY(-400);
+            } else if (this.cursors.down.isDown) {
+                this.player.setVelocityY(400);
+            }
+        }
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'phaser-example',
+    width: window.innerWidth - 20,
+    height: window.innerHeight - 20,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+    },
+    scene: [Game]
 };
+
+var moveCam = false;
 
 const game = new Phaser.Game(config);
